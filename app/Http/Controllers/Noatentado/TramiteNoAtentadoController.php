1ada3e759cd2899494af37110940c1e9fdc83519
Catiel <oliver.alandiaa@gmail.com>
2026-04-30 13:52:16 -0400

    feat: add TramiteNoAtentadoController for No Atentado processing and update SITRA test queries

--- app/Http/Controllers/Noatentado/TramiteNoAtentadoController.php
+++ app/Http/Controllers/Noatentado/TramiteNoAtentadoController.php
@@ -2976,7 +2976,7 @@ class TramiteNoAtentadoController extends Controller
             
             $antiguo=json_encode($tramite);
             SessionController::write('D',$antiguo,'','d_tramitas','8',$tramite->cod_dtra);
-            $this->eliminarUsosRecaudacionPorTramite((int)$tramite->cod_dtra);
+            // $this->eliminarUsosRecaudacionPorTramite((int)$tramite->cod_dtra); // Se comenta para no liberar el control
             $tramite->delete();
             \Session::flash('exito','Se ha eliminado con exito el trámite');
         }
